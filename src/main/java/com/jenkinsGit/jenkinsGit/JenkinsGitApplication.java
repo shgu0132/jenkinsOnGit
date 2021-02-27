e96e7de05a6e0a341b5b6c301c9b38d5ef3e6abf
shgu0132 <g.shivendra@gmail.com>
2021-02-27 18:08:59 +0100

    Jenkins on git

---
+++ src/main/java/com/jenkinsGit/jenkinsGit/JenkinsGitApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsGit.jenkinsGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsGit/jenkinsGit/JenkinsGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsGit.jenkinsGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
